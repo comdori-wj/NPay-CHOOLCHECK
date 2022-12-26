9bc9f8a8844e26c551f31429f04f97504bfaeaec
Wonji Ha <02145s1@gmail.com>
2022-12-27 00:43:15 +0900

    NPayCHOOLCHECK V1.0.5
    - 지정된 시간에 이벤트가 작동되는 기능 구현

--- index.js
+++ index.js
@@ -13,7 +13,8 @@
  * 2022/12/17        wj       포인트 적립 기능 구현
  * 2022/12/20        wj       포인트 적립 기능 구현 및 수정
  * 2022/12/23        wj       3차 광고 추가
- */
+ * 2022/12/27        wj       지정된 시간에 자동 실행 구현
+  */
 
 import puppeteer from 'puppeteer'; //
 import config from './config.js'; // 설정 파일
@@ -42,113 +43,115 @@ if (!config.id || !config.pw) {
 
     const autoWork = schedule.scheduleJob('10 00 10 * * *', () => { // 오전 8시 이벤트
         console.log("네이버 페이 자동 출첵이 시작되었습니다.");
-        // Job();
+        Job(); // 자동화
     })
+    async function Job() {
 
-    const page = (await browser.pages())[0]; // 첫 번째 탭에서 시작.
-    await page.setViewport({
-        width: 1280, height: 1024
-    }); // 화면 크기
+        const page = (await browser.pages())[0]; // 첫 번째 탭에서 시작.
+        await page.setViewport({
+            width: 1280, height: 1024
+        }); // 화면 크기
 
-    await page.goto("https://nid.naver.com/nidlogin.login"); // 로그인 페이지로 이동
-    try {
-        // await page.$x("/html/body/div[1]/div[2]/div/div[1]/form/ul/li/div/div[1]/div[1]/input");
-        // await page.type("#id", config.id);
-        await page.waitForTimeout(1000); // 로그인 페이지 로딩 대기
-        await page.evaluate((id, pw) => { // 아이디, 비밀번호 입력
-            document.querySelector('#id').value = id;
-            document.querySelector('#pw').value = pw;
-        }, config.id, config.pw);
+        await page.goto("https://nid.naver.com/nidlogin.login"); // 로그인 페이지로 이동
+        try {
+            // await page.$x("/html/body/div[1]/div[2]/div/div[1]/form/ul/li/div/div[1]/div[1]/input");
+            // await page.type("#id", config.id);
+            await page.waitForTimeout(1000); // 로그인 페이지 로딩 대기
+            await page.evaluate((id, pw) => { // 아이디, 비밀번호 입력
+                document.querySelector('#id').value = id;
+                document.querySelector('#pw').value = pw;
+            }, config.id, config.pw);
 
-        // await page.$x("/html/body/div[1]/div[2]/div/div[1]/form/ul/li/div/div[1]/div[2]/input");
-        // await page.type("#pw", config.pw);
-        await page.click("#keep"); //로그인 상태 유지 버튼
-    } catch (e) {
-        throw Error("id, pw를 입력하지 못했습니다. 확인해 주십시오: " + e);
-    }
-    try { // 로그인 처리
-        await page.waitForTimeout(3000); // 로그인 처리 대기(봇 방지 처리)
-        await page.click("#log\\.login");
-        await page.waitForTimeout(5000); // 대기
-        await page.screenshot({
-            path: 'Screenshot/loginOk.png', fullPage:false
-        });
-    } catch (e) {
-        await page.waitForTimeout(5000);
-        await page.screenshot({
-            path: 'Screenshot/loginFail.png', fullPage:false
-        });
-        throw Error("로그인 하지 못했습니다. 계정을 확인해 주세요." +e);
-    }
+            // await page.$x("/html/body/div[1]/div[2]/div/div[1]/form/ul/li/div/div[1]/div[2]/input");
+            // await page.type("#pw", config.pw);
+            await page.click("#keep"); //로그인 상태 유지 버튼
+        } catch (e) {
+            throw Error("id, pw를 입력하지 못했습니다. 확인해 주십시오: " + e);
+        }
+        try { // 로그인 처리
+            await page.waitForTimeout(3000); // 로그인 처리 대기(봇 방지 처리)
+            await page.click("#log\\.login");
+            await page.waitForTimeout(5000); // 대기
+            await page.screenshot({
+                path: 'Screenshot/loginOk.png', fullPage: false
+            });
+        } catch (e) {
+            await page.waitForTimeout(5000);
+            await page.screenshot({
+                path: 'Screenshot/loginFail.png', fullPage: false
+            });
+            throw Error("로그인 하지 못했습니다. 계정을 확인해 주세요." + e);
+        }
 
-    ////////////////////1차 광고////////////////////
-    try {
-        await page.goto('https://ofw.adison.co/u/naverpay/ads/55162'); // 오전 8시 마이스토어
+        ////////////////////1차 광고////////////////////
+        try {
+            await page.goto('https://ofw.adison.co/u/naverpay/ads/55162'); // 오전 8시 마이스토어
 
-    }catch (e) {
-        let errorMsg = "1차 적립 페이지에 접속 할 수 없습니다. 다시 확인후 재시도 해주십시오."
-        throw Error(errorMsg + e);
-    }
-    try {
-        await page.waitForTimeout(7000);
-        await page.click("#app > div:nth-child(2) > div > div > div > button"); // 포인트 받기 버튼
-        await page.waitForTimeout(3000);
-        await page.screenshot({
-            path: 'Screenshot/NPayResult1.png', fullPage:false
-        });
-    } catch (e) {
-        await page.waitForTimeout(2000);
-        await page.screenshot({
-            path: 'Screenshot/NPayFail1.png', fullPage:false
-        });
-        throw  Error("포인트 받기 버튼 클릭 실패!" + e);
-    }
+        } catch (e) {
+            let errorMsg = "1차 적립 페이지에 접속 할 수 없습니다. 다시 확인후 재시도 해주십시오."
+            throw Error(errorMsg + e);
+        }
+        try {
+            await page.waitForTimeout(7000);
+            await page.click("#app > div:nth-child(2) > div > div > div > button"); // 포인트 받기 버튼
+            await page.waitForTimeout(3000);
+            await page.screenshot({
+                path: 'Screenshot/NPayResult1.png', fullPage: false
+            });
+        } catch (e) {
+            await page.waitForTimeout(2000);
+            await page.screenshot({
+                path: 'Screenshot/NPayFail1.png', fullPage: false
+            });
+            throw  Error("포인트 받기 버튼 클릭 실패!" + e);
+        }
 
-    ////////////////////2차 광고////////////////////
-    try {
-        await page.waitForTimeout(3000);
-        await page.goto('https://ofw.adison.co/u/naverpay/ads/72557'); // 오전 10시 현장결제
-    }catch (e) {
-        let errorMsg = "2차 적립 페이지에 접속 할 수 없습니다. 다시 확인후 재시도 해주십시오."
-        throw Error(errorMsg + e);
-    }
-    try {
-        await page.waitForTimeout(7000);
-        await page.click("#app > div:nth-child(2) > div > div > div > button"); // 포인트 받기 버튼
-        await page.waitForTimeout(3000);
-        await page.screenshot({
-            path: 'Screenshot/NPayResult2.png', fullPage:false
-        });
-    } catch (e) {
-        await page.waitForTimeout(2000);
-        await page.screenshot({
-            path: 'Screenshot/NPayFail2.png', fullPage:false
-        });
-        throw  Error("포인트 받기 버튼 클릭 실패!" + e);
-    }
+        ////////////////////2차 광고////////////////////
+        try {
+            await page.waitForTimeout(3000);
+            await page.goto('https://ofw.adison.co/u/naverpay/ads/72557'); // 오전 10시 현장결제
+        } catch (e) {
+            let errorMsg = "2차 적립 페이지에 접속 할 수 없습니다. 다시 확인후 재시도 해주십시오."
+            throw Error(errorMsg + e);
+        }
+        try {
+            await page.waitForTimeout(7000);
+            await page.click("#app > div:nth-child(2) > div > div > div > button"); // 포인트 받기 버튼
+            await page.waitForTimeout(3000);
+            await page.screenshot({
+                path: 'Screenshot/NPayResult2.png', fullPage: false
+            });
+        } catch (e) {
+            await page.waitForTimeout(2000);
+            await page.screenshot({
+                path: 'Screenshot/NPayFail2.png', fullPage: false
+            });
+            throw  Error("포인트 받기 버튼 클릭 실패!" + e);
+        }
 
-    ////////////////////3차 광고////////////////////
-    try {
-        await page.waitForTimeout(2000);
-        await page.goto('https://ofw.adison.co/u/naverpay/ads/67823') // 오전 10시 즉시적립
+        ////////////////////3차 광고////////////////////
+        try {
+            await page.waitForTimeout(2000);
+            await page.goto('https://ofw.adison.co/u/naverpay/ads/67823') // 오전 10시 즉시적립
 
-    }catch (e) {
-        let errorMsg = "3차 적립 페이지에 접속 할 수 없습니다. 다시 확인후 재시도 해주십시오."
-        throw Error(errorMsg + e);
-    }
-    try {
-        await page.waitForTimeout(7000);
-        await page.click("#app > div:nth-child(2) > div > div > div > button");
-        await page.waitForTimeout(3000);
-        await page.screenshot({
-            path: 'Screenshot/NPayResult3.png', fullPage:false
-        });
-    } catch (e) {
-        await page.waitForTimeout(2000);
-        await page.screenshot({
-            path: 'Screenshot/NPayFail3.png', fullPage:false
-        });
-        throw  Error("포인트 받기 버튼 클릭 실패!" + e);
+        } catch (e) {
+            let errorMsg = "3차 적립 페이지에 접속 할 수 없습니다. 다시 확인후 재시도 해주십시오."
+            throw Error(errorMsg + e);
+        }
+        try {
+            await page.waitForTimeout(7000);
+            await page.click("#app > div:nth-child(2) > div > div > div > button");
+            await page.waitForTimeout(3000);
+            await page.screenshot({
+                path: 'Screenshot/NPayResult3.png', fullPage: false
+            });
+        } catch (e) {
+            await page.waitForTimeout(2000);
+            await page.screenshot({
+                path: 'Screenshot/NPayFail3.png', fullPage: false
+            });
+            throw  Error("포인트 받기 버튼 클릭 실패!" + e);
+        }
     }
 
 })();
