735ccfe05be7930c459b84982b45c9e6e858f1c2
comdori <02145s1@gmail.com>
2023-02-18 00:55:48 +0900

    Update 원본 마이그레이션 후 이전 버전 복원
    도커 환경변수 항목 config.js로 마이그레이션후 이전 버전 복원(개발용으로 사용될 예정)

--- configDev.js
+++ configDev.js
@@ -10,6 +10,7 @@
  * -----------------------------------------------------------
  * 2023/01/11        wj       최초 생성, 도커 환경변수에서 프로그램 사용 동의서 및 계정 정보를 입력 할 수 있도록 변경.
  * 2023/02/17        wj       동의 항목 주석 제거
+ * 2023/02/18        wj       도커 환경변수 항목 config.js로 마이그레이션후 이전 버전 복원(개발용으로 사용될 예정)
  */
 
 const config = {
@@ -17,7 +18,7 @@ const config = {
     // 이 앱을 사용해서 발생하는 모든 문제는 사용자 본인 책임임을 알려드립니다.
     // 동의하시면 agree 항목의 값을 [true]로 변경하시기 바랍니다.
 
-    agree: process.env.AGREE,
+    agree: true,
     puppeteer: {
         launchOptions: {
             // defaultViewport: null,
@@ -25,7 +26,7 @@ const config = {
             // headless: false, // 브라우저 띄움
             // slowMo: 500,
             executablePath: process.env.CHROMIUM_PATH,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: ['--no-sandbox']
 
         },
         viewportOptions: {
@@ -33,9 +34,8 @@ const config = {
             height: 1024,
         },
     },
-    id: process.env.ID,
-    pw: process.env.PW
-
+    id: '',
+    pw: ''
 }
 
 export default config
